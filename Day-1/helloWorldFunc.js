e86030e42af5d7aa451ef321e54795955ed9dce5
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2026-02-22 21:53:18 +0530

    Code of helloworld function

--- Day-1/helloWorldFunc.js
+++ Day-1/helloWorldFunc.js
@@ -7,18 +7,3 @@ const helloWorldFunc = () => {
 }
 let ans = helloWorldFunc()
 console.log("🚀 ~ ans:", ans());
-
-
-
-
-// # Counter
-
-// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n+1, n+2, etc).
-
-const fn = (n) => {
-    return function counter() {
-        return n++
-    }
-}
-let ans2 = fn(5)
-console.log("🚀 ~ ans2:", ans2());
